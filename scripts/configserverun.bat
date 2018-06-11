5c1fe2b338d4ff073e9c116d25702431e4bb78f5
Andrei Dremov <andrei_dremov@epam.com>
2018-06-12 01:51:16 +0300

    Add config from git repository

--- scripts/configserverun.bat
+++ scripts/configserverun.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-start java -jar -Xmx192m ../simple-config/target/simple-config-1.0-SNAPSHOT.jar
+start java -Dgithub.username=andreitop -Dgithub.password= -jar -Xmx192m ../simple-config/target/simple-config-1.0-SNAPSHOT.jar
